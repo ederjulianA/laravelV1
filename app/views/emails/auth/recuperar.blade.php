1d922f11f80d9e509f5e91bfe70ca6305529d142
ederjulianA <ederalvarez2091057@gmail.com>
2014-03-23 23:34:05 -0500

    actualizacion mail recuperar pass

--- app/views/emails/auth/recuperar.blade.php
+++ app/views/emails/auth/recuperar.blade.php
@@ -1,7 +1,8 @@
 Hola {{ $username }}, <br>
 
 <p> Al parecer has solicitado una nueva contrasena. Te hemos enviado tu nueva contrasena junto
-con un link de activacion. Si no solicitaste una nueva contrasena porfavor has caso omizo de este mensaje</p>
+Con un link de activación. Si no solicitaste una nueva contrasena por favor has caso omiso de este mensaje
+</p>
 
 Nueva Contrasena:  {{ $password }} <br><br>
 
