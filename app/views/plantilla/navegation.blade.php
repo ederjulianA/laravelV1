fe327f207f892ee70448f2dabe822239edd69806
ederjulianA <ederalvarez2091057@gmail.com>
2014-03-23 22:56:09 -0500

    actualizacion del menu
    se actualiza el menu de la aplicacion

--- app/views/plantilla/navegation.blade.php
+++ app/views/plantilla/navegation.blade.php
@@ -1,21 +1,21 @@
-		
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<ul class="nav navbar-nav">
+			
+		<nav class="">
+			
+				<ul class="">
 					<li><a href="{{ URL::route('home')}}">Home</a></li>
 
 
 					@if(Auth::check())
-						<li class="dropdown">
+						
 							
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuario <b class="caret"></b></a>
-							<ul class="dropdown-menu">
+							
+							
 								<li><a href="{{ URL::route('cuenta-sign-out')}}">Cerrar Sesion</a></li>
 								<li><a href="{{ URL::route('cuenta-cambiar-pass')}}">Cambiar Contrasena</a></li>
 								<li><a href="{{ URL::route('cuenta-nit')}}">registrar Nit</a></li>
 
-							</ul>
-						</li>
+							
+						
 						
 
 					@else
@@ -24,5 +24,7 @@
 						<li><a href="{{ URL::route('cuenta-olvido-pass')}}">Olvido Su Contrasena</a></li>
 					@endif
 				</ul>
-			</div>	
+
+	
+				
 		</nav>
