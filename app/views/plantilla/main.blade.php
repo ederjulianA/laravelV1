50aeca87ab8984c4ff92a368995948b4ca3a5597
ederjulianA <ederalvarez2091057@gmail.com>
2014-03-23 23:51:59 -0500

    actualizacion de twitter

--- app/views/plantilla/main.blade.php
+++ app/views/plantilla/main.blade.php
@@ -43,6 +43,12 @@
 	
 	@yield('content')
 	<aside class="contenido-lateral">
+		<center>
+			<a class="twitter-timeline" href="https://twitter.com/ederjulianA" data-widget-id="317728820402716674">Tweets by @ederjulianA</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+		</center>
 	
 	</aside>
 	{{ HTML::script('assets/js/bootstrap.min.js')}}
